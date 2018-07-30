f0a51c7494e39bf16d2cf674a7ac2221fe5270c6
IOLevi <337@holbertonschool.com>
2018-07-30 16:08:25 -0700

    doesn't make sense

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,12 +2,8 @@
 # sets up your web servers for the deployment of web_static
 sudo apt-get -y update
 sudo apt-get install -y nginx
-if [ ! -d /data/web_static/releases/test/ ]; then
-    sudo mkdir -p /data/web_static/releases/test/;
-fi
-if [ ! -d /data/web_static/releases/test/ ]; then
-    sudo mkdir -p /data/web_static/shared/;
-fi
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 boiler="<html>
   <head>
   </head>
