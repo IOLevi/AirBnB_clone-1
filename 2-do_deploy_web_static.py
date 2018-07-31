03eec01579b22e1f49b37111b3046e4e5a9c8a65
IOLevi <337@holbertonschool.com>
2018-07-30 23:43:18 -0700

    autopep8

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,6 +7,7 @@ env.hosts = ['104.196.66.195', '35.237.47.86']
 env.key_filename = "~/.ssh/holberton"
 env.user = "ubuntu"
 
+
 def do_pack():
     "packs a tar archive"
     fn = "web_static_{}.tgz".format(datetime.now().strftime("%Y%m%d%H%M%S"))
@@ -17,6 +18,7 @@ def do_pack():
     else:
         return None
 
+
 def do_deploy(archive_path):
     "deploys new version of  codes"
     ap = archive_path.split("/")[1]
@@ -24,12 +26,15 @@ def do_deploy(archive_path):
     try:
         put(archive_path, "/tmp/{}".format(ap))
         run("mkdir -p /data/web_static/releases/{}".format(apsans))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(ap, apsans))
+        run("tar -xzf /tmp/{} -C\
+                /data/web_static/releases/{}".format(ap, apsans))
         run("rm /tmp/{}".format(ap))
-        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(apsans, apsans))
+        run("mv /data/web_static/releases/{}/web_static/*\
+                /data/web_static/releases/{}/".format(apsans, apsans))
         run("rm -rf /data/web_static/releases/{}/web_static".format(apsans))
         run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(apsans))
+        run("ln -s /data/web_static/releases/{}/\
+                /data/web_static/current".format(apsans))
         print("worked")
         return True
     except BaseException as e:
