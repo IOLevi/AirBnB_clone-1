a3b803af92720e3e4b8cee467c8525037127d009
IOLevi <337@holbertonschool.com>
2018-07-30 22:32:46 -0700

    typo

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,6 +2,8 @@
 from fabric.api import *
 import os
 env.hosts = ['104.196.66.195', '35.237.47.86']
+env.key_filename = "~/.ssh/holberton"
+
 def do_deploy(archive_path):
     "deploys new version of  codes"
     try:
@@ -12,8 +14,8 @@ def do_deploy(archive_path):
         run("tar -xfz /tmp/{} -C /data/web_static/releases/{}".format(ap, apsans))
         run("rm /tmp/{}".format(ap))
         run("mv /data/web_static_releases/{}/web_static/* /data/web_static/releases/{}/".format(apsans, apsans))
-        run("rm -rf /data/web_static/releases/{}}/web_static".format(apsans))
-        run ("rm -rf /data/web_static/current")
+        run("rm -rf /data/web_static/releases/{}/web_static".format(apsans))
+        run("rm -rf /data/web_static/current")
         run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(apsans))
         return True
     except:
