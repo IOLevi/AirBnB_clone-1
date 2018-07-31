181a1a704590574341e581e8d2ce12778b017a79
IOLevi <337@holbertonschool.com>
2018-07-30 23:45:57 -0700

    3 with working 2

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,4 +1,4 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 from fabric.api import *
 from datetime import datetime
 import os
@@ -6,6 +6,7 @@ import os
 env.hosts = ['104.196.66.195', '35.237.47.86']
 env.key_filename = "~/.ssh/holberton"
 env.user = "ubuntu"
+
 
 def do_pack():
     "packs a tar archive"
@@ -17,6 +18,7 @@ def do_pack():
     else:
         return None
 
+
 def do_deploy(archive_path):
     "deploys new version of  codes"
     ap = archive_path.split("/")[1]
@@ -24,20 +26,25 @@ def do_deploy(archive_path):
     try:
         put(archive_path, "/tmp/{}".format(ap))
         run("mkdir -p /data/web_static/releases/{}".format(apsans))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}".format(ap, apsans))
+        run("tar -xzf /tmp/{} -C\
+                /data/web_static/releases/{}".format(ap, apsans))
         run("rm /tmp/{}".format(ap))
-        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(apsans, apsans))
+        run("mv /data/web_static/releases/{}/web_static/*\
+                /data/web_static/releases/{}/".format(apsans, apsans))
         run("rm -rf /data/web_static/releases/{}/web_static".format(apsans))
         run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(apsans))
+        run("ln -s /data/web_static/releases/{}/\
+                /data/web_static/current".format(apsans))
+        print("worked")
         return True
-    except:
+    except BaseException as e:
+        print(e)
         return False
-        
+
 def deploy():
-    "calls do_pack and do_deploy"
-    p = do_pack()
-    if p is None:
-        return False
-    a = do_deploy(p)
-    return a
+    "deploy"
+    a = do_pack()
+        if a is None:
+            return False
+        else:
+            return do_deploy(a)
