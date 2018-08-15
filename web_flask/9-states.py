6a2a255c486c655687ccb62a4b18deeab94c2387
IOLevi <337@holbertonschool.com>
2018-08-15 23:43:14 +0000

    through 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -49,7 +49,7 @@ def get_state():
         a = storage.all("State")
 
     a = list(a.values())
-    return render_template('9-states.py', a=a)
+    return render_template('9-states.html', a=a)
 
 
 @app.route('/states/<path:id>', strict_slashes = False)
@@ -69,7 +69,7 @@ def get_state_id(id):
         if state.id == id:
             b.append(state)
 
-    return render_templates('9-states.py', b=b)
+    return render_template('9-states.html', b=b)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
